0b1352db3d25456b456013832c7ef6d9b4aaad9b
KylonBlevins <kylonjblevins@gmail.com>
2025-09-22 11:22:45 -0500

    Functions
    added toLower, toUpper, strLength, Append, Prepend, EqualTo, and Cstr functions

--- classes.h
+++ classes.h
@@ -1,1 +1,6 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "functions.h"
+using namespace std;
--- functions.cpp
+++ functions.cpp
@@ -1,4 +1,67 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include "functions.h"
+#include "classes.h"
+using namespace std;
 
+string toLower(string input)
+{
+	for (int i = 0; i < input.length(); i++)
+	{
+		if (input[i] >= 'A' && input[i] <= 'Z')
+		{
+			input[i] = input[i] + 32;
+		}
+	}
+	cout << input;
+	return input;
+}
+
+string toUpper(string input)
+{
+	for (int i = 0; i < input.length(); i++)
+	{
+		if (input[i] >= 'a' && input[i] <= 'z')
+		{
+			input[i] = input[i] - 32;
+		}
+	}
+	cout << input;
+	return input;
+}
+
+int strLength(string input)
+{
+	return input.length();
+}
+
+string Append(string input)
+{
+	input = input + "str";
+	return input;
+}
+
+string Prepend(string input)
+{
+	input = "str" + input;
+	return input;
+}
+
+bool EqualTo(string input1, string input2)
+{
+	if (input1 == input2)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+char CStr(string input1)
+{
+	const char strCon = input1[1];
+	return strCon;
+}
